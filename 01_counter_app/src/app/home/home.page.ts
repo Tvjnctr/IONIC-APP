dac4093e7aeb0338e4b1fdc8bb7a2cf8bf4292f9
Brian Bernales <br.beranales@duocuc.cl>
2023-11-01 23:52:03 -0300

    rut validacion

--- 01_counter_app/src/app/home/home.page.ts
+++ 01_counter_app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,32 +7,73 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  rutInput: string;
+  isInputValid: boolean = true;
 
-  public n: number;
-  public showNumber: string;
-
-  constructor() {
-    this.n = 0;
-    this.showNumber = '00';
+  constructor() {}
+  onRutInputChange() {
+    // Verifica si el valor del campo de entrada contiene solo números
+    this.isInputValid = /^[0-9Kk\-]+$/.test(this.rutInput);
   }
-
-  up(){
-    this.n++;
-    this.formatShowNumber();
-  }
-
-
-  down(){
-    this.n--
-    this.formatShowNumber();
-  }
-
-  formatShowNumber(){
-    if(this.n < 10) {
-      this.showNumber = '0' + this.n;
+  validateRut() {
+    // Agregar lógica para validar el RUT aquí
+    const rut = this.rutInput;
+    if (this.isValidRut(rut)) {
+      // RUT válido
+      console.log('RUT válido');
     } else {
-      this.showNumber = '' + this.n;
+      // RUT inválido
+      console.log('RUT inválido');
     }
   }
 
+  isValidRut(rut: string): boolean {
+    // Expresión regular para validar RUT sin punto ni guión con dígito verificador
+    const rutPattern = /^\d{7,8}-?(\d|k|K)$/;
+    if (!rutPattern.test(rut)) {
+      return false; // El formato del RUT es incorrecto
+    }
+  
+    // Extraer el número y el dígito verificador
+    const [rutNumber, rutDv] = rut.split('-');
+    // Validar el dígito verificador
+    return this.calculateDv(rutNumber) === rutDv.toUpperCase();
+  }
+  
+  calculateDv(rutNumber: string): string {
+    // Elimina cualquier posible punto o guión en el RUT
+    rutNumber = rutNumber.replace(/\./g, '').replace(/-/g, '');
+  
+    // Convierte el RUT a un número entero
+    let rut = parseInt(rutNumber, 10); // Cambiamos 'const' a 'let'
+  
+    // Calcula el dígito verificador
+    let suma = 0;
+    let multiplicador = 2;
+  
+    while (rut > 0) {
+      const digito = rut % 10;
+      suma += digito * multiplicador;
+  
+      rut = Math.floor(rut / 10);
+  
+      multiplicador++;
+      if (multiplicador > 7) {
+        multiplicador = 2;
+      }
+    }
+  
+    const dvCalculado = 11 - (suma % 11);
+  
+    // En caso de que el DV sea 11, lo reemplazamos por '0'
+    if (dvCalculado === 11) {
+      return '0';
+    }
+    // En caso de que el DV sea 10, lo reemplazamos por 'K'
+    if (dvCalculado === 10) {
+      return 'K';
+    }
+  
+    return dvCalculado.toString();
+  }
 }
